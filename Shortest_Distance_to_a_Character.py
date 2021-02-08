c8d32ad1d9f7348248936f0e706426c2ea135826
bhuvaneswarignanasekar <bhuvaneswarignanasekar@users.noreply.github.com>
2021-02-07 19:52:28 -0600

    Create Shortest_Distance_to_a_Character.py

---
+++ Shortest_Distance_to_a_Character.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  7 19:49:47 2021
+
+@author: shanm
+"""
+
+import numpy as np
+class Solution:
+    def shortestToChar(self, s: str, c: str) -> List[int]:
+        char_ind=[]
+        res=[]
+        
+        for index in range(len(s)):
+            if c is s[index]:
+                char_ind.append(index)
+        for i in range(len(s)):
+            closest=self.nearest(char_ind,i)
+            res.append(abs(i-closest))
+        return res
+        
+    def nearest(self,array,val):
+        array = np.asarray(array)
+        ind = (np.abs(array - val)).argmin()
+        return(array[ind])
+            
+                    
