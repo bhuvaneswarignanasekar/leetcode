25cfb009ae830102c7fc287c0a1dacb84909b49b
bhuvaneswarignanasekar <bhuvaneswarignanasekar@users.noreply.github.com>
2021-02-11 08:40:39 -0600

    Create implement_strStr().py

---
+++ implement_strStr().py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  7 19:49:47 2021
+
+@author: bhuvaneswari
+"""
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        return haystack.find(needle)
+        
