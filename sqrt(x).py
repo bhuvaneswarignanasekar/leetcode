1e411a334753b64bd79fd3b3265a7b5200bee520
bhuvaneswarignanasekar <bhuvaneswarignanasekar@users.noreply.github.com>
2021-02-18 20:21:03 -0600

    leet_code

---
+++ sqrt(x).py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  7 19:49:47 2021
+
+@author: bhuvaneswari
+"""
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        return int(sqrt(x))
+        
+        
+        
+        
