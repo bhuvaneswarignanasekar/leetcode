acb92af580eedbfdda439aa7971bdaf50fd9cba8
bhuvaneswarignanasekar <59138503+bhuvaneswarignanasekar@users.noreply.github.com>
2021-04-27 15:39:13 -0500

    leetcode

--- powerofthree.py
+++ powerofthree.py
@@ -5,3 +5,11 @@ class Solution:
             n=n/3
         if n==1:return True
         return False
+
+class Solution2:
+    def isPowerOfThree(self, n: int) -> bool:
+        hi_po=1162261467
+        if n<=0:return False
+        if hi_po%n==0:return True
+        return False
+        
